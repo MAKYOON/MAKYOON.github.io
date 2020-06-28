2eb48f6c3da545b5df4f8591833e3e5c64cee28e
MAKYOON <k.li@ludus-academie.com>
2020-06-28 23:31:26 +0200

    Added simple navbar dropdown through CSS / And added underline animation when hovering on navbar links

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-const dropdowns = document.querySelectorAll('.dropdownbtn');
-const dropdownElements = document.querySelectorAll('.dropdown-content');
-
-dropdowns[0].addEventListener("mouseover",function(e) { console.log (e.target)});
-
-
-
-
+// const dropdowns = document.querySelectorAll('.dropdownbtn');
+// const dropdownElements = document.querySelectorAll('.dropdown-content');
+//
+// dropdowns[0].addEventListener("mouseover",function(e) { console.log (e.target)});
+//
+//
+//
+//
