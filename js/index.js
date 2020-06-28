e9e64c84d38b1286cec04a1e2a8d7b9f9610aaa6
MAKYOON <k.li@ludus-academie.com>
2020-06-28 10:09:08 +0200

    - Added comments to html/css - Cleaned a few things in scss file
    Still TODO : Navbar dropdown

--- js/index.js
+++ js/index.js
@@ -1,11 +1,8 @@
-const displayDropDown = (e) => {
-    console.log(e.name);
-}
-
 const dropdowns = document.querySelectorAll('.dropdownbtn');
 const dropdownElements = document.querySelectorAll('.dropdown-content');
 
-dropdowns[0].addEventListener("mouseover",displayDropDown());
+dropdowns[0].addEventListener("mouseover",function(e) { console.log (e.target)});
+
 
 
 
