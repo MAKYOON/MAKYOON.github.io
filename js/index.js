ecbc82db597fc0329725d0e00248466d8352bda2
MAKYOON <k.li@ludus-academie.com>
2020-07-06 12:11:46 +0200

    Refonte de la navbar (HTML + CSS) / encore à fixer la navbar mobile

--- js/index.js
+++ js/index.js
@@ -1,22 +1,31 @@
 const hamburger = document.querySelector('#hamburger');
-const nav = document.querySelector('nav');
+const nav = document.querySelector('#navbar-items');
 const body = document.querySelector('body');
 const dropdownbtn = document.querySelectorAll('.dropdownbtn');
 const dropdowncontents = document.querySelectorAll('.dropdown-content');
 
+console.log(nav);
+
 //handling the navbar in small medias : instead of hovering we need to click
 dropdownbtn.forEach((element, index) => {
     element.addEventListener('click', () => {
-        dropdowncontents[index].classList.toggle('dropdown-content');
-        dropdowncontents[index].classList.toggle('dropdown-click');
+        console.log(document.body.clientWidth);
+        if (document.body.clientWidth < 1150) {
+            dropdowncontents[index].classList.toggle('dropdown-content');
+            dropdowncontents[index].classList.toggle('dropdown-click');
+        }
     })
 })
 
 //toggling the nav-active class which plays the animation of fading in the dropdown in and out
-hamburger.addEventListener('click', () => {
-    console.log("clicked");
-    nav.classList.toggle('nav-active');
-});
+const toggleNav = () => {
+    hamburger.addEventListener('click', () => {
+        console.log("clicked");
+        nav.classList.toggle('nav-active');
+    });
+}
+
+toggleNav();
 
 //this is so we can get rid of the dropdown by clicking anywhere else on the page
 //we have to check for the target ID because if we click on the burger it runs both listeners and so nothing happens
