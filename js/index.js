0fc17f563386092615dbaaf4737ac1e95e7a3ac9
MAKYOON <k.li@ludus-academie.com>
2020-07-07 18:34:48 +0200

    Fix de la navbar responsive ! Les textes sont bien placés maintenant lorsqu'on clique sur les dropdowns / TODO : Une petite flèche sur les dropdowns + animation sur l'hamburger

--- js/index.js
+++ js/index.js
@@ -4,12 +4,9 @@ const body = document.querySelector('body');
 const dropdownbtn = document.querySelectorAll('.dropdownbtn');
 const dropdowncontents = document.querySelectorAll('.dropdown-content');
 
-console.log(nav);
-
 //handling the navbar in small medias : instead of hovering we need to click
 dropdownbtn.forEach((element, index) => {
     element.addEventListener('click', () => {
-        console.log(document.body.clientWidth);
         if (document.body.clientWidth < 1150) {
             dropdowncontents[index].classList.toggle('dropdown-content');
             dropdowncontents[index].classList.toggle('dropdown-click');
@@ -17,26 +14,29 @@ dropdownbtn.forEach((element, index) => {
     })
 })
 
-//toggling the nav-active class which plays the animation of fading in the dropdown in and out
-const toggleNav = () => {
-    hamburger.addEventListener('click', () => {
-        console.log("clicked");
-        nav.classList.toggle('nav-active');
-    });
-}
 
-toggleNav();
+
+//toggling the nav-active class which plays the animation of fading in the dropdown in and out
+hamburger.addEventListener('click', () => {
+    console.log("clicked");
+    nav.classList.toggle('nav-inactive');
+    nav.classList.toggle('nav-active');
+});
 
 //this is so we can get rid of the dropdown by clicking anywhere else on the page
 //we have to check for the target ID because if we click on the burger it runs both listeners and so nothing happens
 body.addEventListener('click', (e) => {
     if (e.target.id != "hamburger" && !e.target.classList.contains('dropdownbtn') && e.target.id != "navbar") {
-        nav.classList.remove('nav-active');
+        nav.classList.toggle('nav-inactive');
+        nav.classList.toggle('nav-active');
         dropdowncontents.forEach(element => {
             element.classList.remove('dropdown-click');
+            element.classList.add('dropdown-content');
         })
     }
 });
+
+
 
 //this is to make sure the animation doesnt play when resizing the page
 let resizeTimer;
