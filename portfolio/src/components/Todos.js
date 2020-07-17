d163db7b8f10f8b77b169c284e4a05fb64a39079
MAKYOON <k.li@ludus-academie.com>
2020-07-17 16:47:07 +0200

    Following react training course

---
+++ portfolio/src/components/Todos.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Todoitem from "./Todoitem";
+import PropTypes from 'prop-types';
+
+class Todos extends Component {
+    render() {
+        return this.props.todos.map((todo) => (
+            <Todoitem todo={todo} key={todo.id}/>
+        ))
+    }
+}
+
+//PropTypes
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
+}
+
+export default Todos;
