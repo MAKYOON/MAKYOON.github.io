e6ec5f62d2e89c1a68c921e45d3eb914fabe4fae
MAKYOON <k.li@ludus-academie.com>
2020-08-22 15:01:19 +0200

    Updates

---
+++ precache-manifest.7a3a3f78f37185fd7678466b387fd759.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b3747bf160aac3302200afd2232850",
+    "url": "/index.html"
+  },
+  {
+    "revision": "784a4544769a97e3e3d4",
+    "url": "/static/css/main.b34ccb32.chunk.css"
+  },
+  {
+    "revision": "1e7f51928bbb45fb78dd",
+    "url": "/static/js/2.25de082a.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.25de082a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784a4544769a97e3e3d4",
+    "url": "/static/js/main.59ed0f86.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "33b3d54c983dc6ec33402647f8348fae",
+    "url": "/static/media/CV_LI_Kevin.33b3d54c.pdf"
+  },
+  {
+    "revision": "c9cb799d5eae477db6bf48bd8d69aa86",
+    "url": "/static/media/accueil.c9cb799d.jpg"
+  },
+  {
+    "revision": "5e454630c0e8fb78a84de7a1f696c550",
+    "url": "/static/media/contact.5e454630.jpg"
+  },
+  {
+    "revision": "5c4e16ceb77211d7b286e351e272328d",
+    "url": "/static/media/css_image.5c4e16ce.png"
+  },
+  {
+    "revision": "59ce65ebd062fb9f95eb61298d5167ed",
+    "url": "/static/media/geekpotato.59ce65eb.png"
+  },
+  {
+    "revision": "5bd5ac0e1b2731d7ad24dd1b962e1811",
+    "url": "/static/media/js_image.5bd5ac0e.png"
+  },
+  {
+    "revision": "4a270589d9d2784f66d866ae45a8a687",
+    "url": "/static/media/photo.4a270589.jpg"
+  },
+  {
+    "revision": "b9d0d764196f4fed959ff7598959009a",
+    "url": "/static/media/portfolio.b9d0d764.png"
+  },
+  {
+    "revision": "d6bdcfb24f57bffcd8e8b695f75ef263",
+    "url": "/static/media/react_image.d6bdcfb2.png"
+  },
+  {
+    "revision": "5accfd27e574d0052d894bc34e6083ed",
+    "url": "/static/media/serenity.5accfd27.png"
+  },
+  {
+    "revision": "8c93311f5f146dd556b442122756cb19",
+    "url": "/static/media/survivalz.8c93311f.png"
+  }
+]);
