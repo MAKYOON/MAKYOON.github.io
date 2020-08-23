c17a36c24f7d325b1f1bb83c53bc06a96cb11a6f
MAKYOON <k.li@ludus-academie.com>
2020-08-23 16:16:17 +0200

    Made modifications to formation

--- portfolio/src/Views/Formation.js
+++ portfolio/src/Views/Formation.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBook} from "@fortawesome/free-solid-svg-icons";
+import {faBriefcase} from "@fortawesome/free-solid-svg-icons";
+import {faChalkboardTeacher} from "@fortawesome/free-solid-svg-icons";
+import {faBreadSlice} from "@fortawesome/free-solid-svg-icons";
 
-function Formation(props)
+function Formation()
 {
 	return (
 		<section
@@ -26,94 +29,90 @@ function Formation(props)
 			</h1>
 			<VerticalTimeline>
 				<VerticalTimelineElement
-					className="vertical-timeline-element--work"
-					contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-					contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-					date="2011 - present"
-					iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+					contentStyle={{ borderTop: '3px solid #5D001E'}}
+					contentArrowStyle={{ borderRight: '7px solid  #5D001E' }}
+					iconStyle={{ background: '#5D001E', color: '#fff' }}
+					icon={<FontAwesomeIcon
+						icon={faBriefcase}
+						className="text-2xl"
+					/>
+					}
 				>
-					<h3 className="vertical-timeline-element-title">Creative Director</h3>
-					<h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+					<div
+						className="m-auto"
+					>
+						<img src={require("../Assets/aircampus.png")}
+							 alt="AirCampus"
+							 className="w-2/5 inline mb-5 mr-8"
+						/>
+						<img src={require("../Assets/unistra.png")}
+							 alt="Université de Strasbourg"
+							 className="w-2/5 inline mb-5 "
+						/>
+					</div>
+					<h3 className="text-xl font-bold">Licence Pro</h3>
+					<h4 className="text-base italic mb-5">Développement Web, Communication et Apprentissages</h4>
+					<div
+						className="text-base mb-3"
+					>
+						Alternance au sein de l'entreprise AirCampus
+					</div>
+					<div
+						className="text-base text-gray-700"
+					>
+						Sept 2020
+					</div>
+				</VerticalTimelineElement>
+				<VerticalTimelineElement
+					contentStyle={{ borderTop: '3px solid #420044'}}
+					contentArrowStyle={{ borderRight: '7px solid #420044' }}
+					date="2018 - 2020"
+					iconStyle={{ background: '#420044', color: '#fff' }}
+					icon={<FontAwesomeIcon
+						icon={faChalkboardTeacher}
+						className="text-xl"
+					/>}
+				>
+					<div>
+						<img src={require("../Assets/ludus.png")}
+							 alt="Ludus Académie"
+							 className="w-2/5 m-auto mb-5 "
+						/>
+					</div>
+					<h3 className="text-xl font-bold">Foundation Degree</h3>
+					<h4 className="text-base italic mb-5">Jeux vidéos et Serious Games</h4>
 					<p>
-						Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+						Préparation d'un Foundation Degree (Bac +2) au sein de l'école Ludus Académie
 					</p>
 				</VerticalTimelineElement>
 				<VerticalTimelineElement
-					className="vertical-timeline-element--work"
-					date="2010 - 2011"
-					iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-
+					contentStyle={{ borderTop: '3px solid #004827'}}
+					contentArrowStyle={{ borderRight: '7px solid #004827' }}
+					date="2016-2018"
+					iconStyle={{ background: '#004827', color: '#fff' }}
+					icon={<FontAwesomeIcon
+						icon={faBreadSlice}
+						className="text-xl"
+					/>}
 				>
-					<h3 className="vertical-timeline-element-title">Art Director</h3>
-					<h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+					<div
+						className="m-auto"
+					>
+						<img src={require("../Assets/amandecannelle.jpg")}
+							 alt="Amande & Cannelle"
+							 className="w-2/5 inline mb-5 mr-8"
+						/>
+						<img src={require("../Assets/cfaeschau.png")}
+							 alt="CFA Eschau"
+							 className="w-2/5 inline mb-5 "
+						/>
+					</div>
+					<h3 className="text-xl font-bold">CAP</h3>
+					<h4 className="text-base italic mb-5">Pâtissier</h4>
 					<p>
-						Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+						Alternance au sein de la pâtisserie Amande & Cannelle pendant 2 ans
 					</p>
 				</VerticalTimelineElement>
-				<VerticalTimelineElement
-					className="vertical-timeline-element--work"
-					date="2008 - 2010"
-					iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-
-				>
-					<h3 className="vertical-timeline-element-title">Web Designer</h3>
-					<h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
-					<p>
-						User Experience, Visual Design
-					</p>
-				</VerticalTimelineElement>
-				<VerticalTimelineElement
-					className="vertical-timeline-element--work"
-					date="2006 - 2008"
-					iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-
-				>
-					<h3 className="vertical-timeline-element-title">Web Designer</h3>
-					<h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
-					<p>
-						User Experience, Visual Design
-					</p>
-				</VerticalTimelineElement>
-				<VerticalTimelineElement
-					className="vertical-timeline-element--education"
-					date="April 2013"
-					iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-
-				>
-					<h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-					<h4 className="vertical-timeline-element-subtitle">Online Course</h4>
-					<p>
-						Strategy, Social Media
-					</p>
-				</VerticalTimelineElement>
-				<VerticalTimelineElement
-					className="vertical-timeline-element--education"
-					date="November 2012"
-					iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-
-				>
-					<h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-					<h4 className="vertical-timeline-element-subtitle">Certification</h4>
-					<p>
-						Creative Direction, User Experience, Visual Design
-					</p>
-				</VerticalTimelineElement>
-				<VerticalTimelineElement
-					className="vertical-timeline-element--education"
-					date="2002 - 2006"
-					iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-
-				>
-					<h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-					<h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
-					<p>
-						Creative Direction, Visual Design
-					</p>
-				</VerticalTimelineElement>
-				<VerticalTimelineElement
-					iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-
-				/>
 			</VerticalTimeline>
 		</section>
 	);
